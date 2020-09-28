eafd43b288d08ba01e43df9de6f2076c42361c30
adamisrusty <adamgutierreztx@gmail.com>
2020-09-27 20:34:41 -0500

    began modularization

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,52 @@
+use http::request::Request;
+use server::Server;
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
+mod server {
+    pub struct Server {
+        addr: String,
     }
 
-    fn run(self) {
-        println!("Listening on {}....", self.addr);
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}....", self.addr);
+        }
     }
 }
+
+mod http {
+
+    pub mod request {
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: super::method::Method,
+        }
+    }
+    pub mod method {
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
+    }
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
